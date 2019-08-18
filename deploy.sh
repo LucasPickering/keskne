d00495517386f82effc6a58dd37721de8f8b41d0
Lucas Pickering <lucas@lucaspickering.me>
2019-08-18 17:05:00 -0400

    Fix RPS websocket proxying

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -e
-
 # Make sure there's a docker host set
-docker-machine active
+docker-machine active > /dev/null 2> /dev/null
 if [ $? -ne 0 ]; then
     echo "No docker-machine set! Is this really what you meant to do?"
     exit 1
 fi
+
+set -ex
 
 docker-compose pull $@
 docker-compose down
