fc945cf542242c222c2a2efed2de40b8f8817ddb
Lucas Pickering <lucas@lucaspickering.me>
2019-08-28 21:37:00 -0400

    Make secrets management more modular

--- postgres/create-dbs.sh
+++ postgres/create-dbs.sh
@@ -6,7 +6,7 @@ set -u
 
 function create_user_and_database() {
     local database=$1
-    local password=$(echo $2) # Read pw from the file
+    local password=$2
     echo "  Creating user '$database' and database '$database'"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
         CREATE USER $database WITH PASSWORD '$password';
@@ -16,6 +16,16 @@ EOSQL
     echo "  Done"
 }
 
+# This code below is untested. I guess we won't really know if it works until
+# the next time the DB volume gets recreated
+
+# Read each secret file into an env var
+for f in ${SECRETS_DIR-"/run/secrets/"}*; do
+    if [ -e "$f" ]; then
+        echo "Reading \"$f\" into \"$f\""
+        $f=$(cat $f) # Load the secret value
+    fi
+done
 echo "Creating users and databases"
-create_user_and_database $MBTA_DB $MBTA_DB_PASSWORD_FILE
-create_user_and_database $RPS_DB $RPS_DB_PASSWORD_FILE
+create_user_and_database $MBTA_DB $keskne_mbta_db_password
+create_user_and_database $RPS_DB $keskne_rps_db_password
