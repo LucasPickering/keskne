c1aa9668988580540226f2e2813e64c65abc11ec
Lucas Pickering <lucas@lucaspickering.me>
2019-10-07 15:48:11 -0400

    Fix nginx scripts

--- revproxy/cmd.sh
+++ revproxy/cmd.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 set -e
-cp nginx.conf /etc/nginx/nginx.conf
+echo "Filling site template confs..."
 (cd sites; for f in *; do envsubst '${ROOT_HOSTNAME} ${RPS_HOSTNAME} ${MBTA_HOSTNAME}' < $f > /etc/nginx/conf.d/$f; done)
-nginx -g "daemon off;"
+
+# Allow for placeholder keys in development
+if [ $(expr length "$REVPROXY_AMPLIFY_API_KEY") -lt 5  ]; then
+    echo "No Amplify API key specified, not starting it"
+    nginx -g "daemon off;"
+else
+    # Amplify image's author doesn't know the difference between entrypoint and cmd
+    # the "entrypoint" script starts nginx and waits for it to exit
+    AMPLIFY_IMAGENAME=${ROOT_HOSTNAME} API_KEY=${REVPROXY_AMPLIFY_API_KEY} /entrypoint.sh $@
+fi
+
--- revproxy/entrypoint.sh
+++ revproxy/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 PREFIX=keskne_ source /app/load_secrets.sh
-# Allow for placeholder keys in development
-if [ $(expr length "$REVPROXY_AMPLIFY_API_KEY") -lt 5  ]; then
-    echo "No Amplify API key specified, not starting it"
-    exec $@
-else
-    AMPLIFY_IMAGENAME=${ROOT_HOSTNAME} API_KEY=${REVPROXY_AMPLIFY_API_KEY} /entrypoint.sh $@
-fi
+exec $@
